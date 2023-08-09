58af6f45c069572691606a1a5f223c373848a8da
SachinSilva99 <sachinmanathunga@gmail.com>
2023-08-09 16:34:18 +0530

    added swagger ui

--- src/main/java/lk/probidder/ProbidderApplication.java
+++ src/main/java/lk/probidder/ProbidderApplication.java
@@ -3,11 +3,12 @@ package lk.probidder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProbidderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProbidderApplication.class, args);
 	}
-
+//	http://localhost:8000/swagger-ui/index.html
 }
--- src/main/java/lk/probidder/api/UserController.java
+++ src/main/java/lk/probidder/api/UserController.java
@@ -1,23 +1,17 @@
 package lk.probidder.api;
 
-import lk.probidder.dto.UserDTO;
 import lk.probidder.dto.request.UserRequestDTO;
 import lk.probidder.dto.response.UserResponseDTO;
-import lk.probidder.entity.User;
 import lk.probidder.service.UserService;
-import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 /*
 Author : Sachin Silva
 */
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -32,10 +26,8 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getUser(@PathVariable Long id) {
-        UserResponseDTO user = null;
         try {
-            user = userService.getUserById(id);
-            return ResponseEntity.ok(user);
+            return ResponseEntity.ok(userService.getUserById(id));
         } catch (ClassNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
